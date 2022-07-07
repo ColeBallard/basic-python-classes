6f724dfa04c4d341e9bd84e7e953a5ae094336d5
Cole Ballard <cole.ballard97@gmail.com>
2022-07-07 12:11:36 -0500

    finished inheritance section

--- main.py
+++ main.py
@@ -2,22 +2,10 @@ import random
 import time
 
 class Animal:
-    def __init__(self, name, species, mammal):
-        self._name = name
+    def __init__(self, species, hungry, mammal):
         self._species = species
         self._mammal = mammal
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, new_name):
-        self._name = new_name
-
-    @name.deleter
-    def name(self):
-        del self._name
+        self.__hungry = hungry
 
     @property
     def species(self):
@@ -30,6 +18,18 @@ class Animal:
     @species.deleter
     def species(self):
         del self._species
+    
+    @property
+    def hungry(self):
+        return self.__hungry
+
+    @hungry.setter
+    def hungry(self, new_hungry):
+        self.__hungry = new_hungry
+
+    @hungry.deleter
+    def hungry(self):
+        del self.__hungry
 
     @property
     def mammal(self):
@@ -44,7 +44,8 @@ class Animal:
         del self._mammal
 
     def __lookForFood(self):
-        if self._mammal:
+        chance = random.randint(0, 1)
+        if chance == 0:
             return True
         else:
             return False
@@ -52,15 +53,34 @@ class Animal:
     def eat(self):
         success = self.__lookForFood()
         if success:
-            print("I'm full!")
+            self.__hungry = False
         else:
-            print("I'm still hungry.")
+            self.__hungry = True
 
-class Book:
-    def __init__(self, name, genre):
+class Fish(Animal):
+    def __init__(self, species, hungry, mammal = False):
+        super().__init__(self, species, hungry, mammal)
+
+    def swim(self):
+        if self.__hungry:
+            print("You didn't get very far...")
+        else:
+            print("You found your son!")
+
+class Snake(Animal):
+    def __init__(self, species, hungry, mammal = False):
+        super().__init__(self, species, hungry, mammal)
+
+    def slither(self):
+        if self.__hungry:
+            print("You didn't get very far...")
+        else:
+            print("You found Harry Potter!")
+
+class Person(Animal):
+    def __init__(self, species, hungry, name, mammal = True):
+        super().__init__(self, species, hungry, mammal)
         self._name = name
-        self._genre = genre
-        self.__pages = 0
 
     @property
     def name(self):
@@ -74,50 +94,124 @@ class Book:
     def name(self):
         del self._name
 
-    @property
-    def genre(self):
-        return self._genre
+    def exercise(self):
+        if self.__hungry:
+            print("You should eat something first...")
+        else:
+            print("Strength +1")
 
-    @genre.setter
-    def genre(self, new_genre):
-        self._genre = new_genre
-
-    @genre.deleter
-    def genre(self):
-        del self._genre
+class Book:
+    def __init__(self, pages):
+        self._pages = pages
 
     @property
     def pages(self):
-        return self.__pages
+        return self._pages
 
     @pages.setter
     def pages(self, new_pages):
-        self.__pages = new_pages
+        self._pages = new_pages
 
     @pages.deleter
     def pages(self):
-        del self.__pages
+        del self._pages
 
     def readTime(self):
-        return str(self.__pages * 3) + " minutes"
+        return str(self._pages * 3) + " minutes"
 
-class Vehicle:
-    def __init__(self, name, make, model):
-        self._name = name
-        self._make = make
-        self._model = model
+class Textbook(Book):
+    def __init__(self, pages, year, subject):
+        super().__init__(self, pages)
+        self._year = year
+        self._subject = subject
 
     @property
-    def name(self):
-        return self._name
+    def year(self):
+        return self._year
 
-    @name.setter
-    def name(self, new_name):
-        self._name = new_name
+    @year.setter
+    def year(self, new_year):
+        self._year = new_year
 
-    @name.deleter
-    def name(self):
-        del self._name
+    @year.deleter
+    def year(self):
+        del self._year
+
+    @property
+    def subject(self):
+        return self._subject
+
+    @subject.setter
+    def subject(self, new_subject):
+        self._subject = new_subject
+
+    @subject.deleter
+    def subject(self):
+        del self._subject
+
+    def slam(self):
+        if self._subject.upper() == "MATH":
+            print("Numbers and equations come flying out of the top of the textbook.")
+        else:
+            print("Learning is hard!")
+
+class AddressBook(Book):
+    def __init__(self, pages, year, location):
+        super().__init__(self, pages)
+        self._year = year
+        self._location = location
+
+    @property
+    def year(self):
+        return self._year
+
+    @year.setter
+    def year(self, new_year):
+        self._year = new_year
+
+    @year.deleter
+    def year(self):
+        del self._year
+
+    @property
+    def location(self):
+        return self._location
+
+    @location.setter
+    def location(self, new_location):
+        self._location = new_location
+
+    @location.deleter
+    def location(self):
+        del self._location
+
+    def throwAway():
+        print("Who uses these things anyways?")
+
+class Vehicle:
+    def __init__(self, wheels):
+        self._wheels = wheels
+
+    @property
+    def wheels(self):
+        return self._wheels
+
+    @wheels.setter
+    def wheels(self, new_wheels):
+        self._wheels = new_wheels
+
+    @wheels.deleter
+    def wheels(self):
+        del self._wheels
+
+    def honk():
+        print("Honk.")
+
+class Car(Vehicle):
+    def __init__(self, make, model, wheels = 4):
+        super().__init__(self, wheels)
+        self._make = make
+        self._model = model
 
     @property
     def make(self):
@@ -156,7 +250,7 @@ class Vehicle:
         print("Braking.")
 
     def drive(self):
-        for i in range(100):
+        for i in range(10):
             choice = random.randint(0,3)
             if choice == 0:
                 self.__turnLeft()
@@ -167,3 +261,98 @@ class Vehicle:
             elif choice == 3:
                 self.__brake()
             time.sleep(1)
+
+class Bicycle(Vehicle):
+    def __init__(self, mountainBike, wheels = 2):
+        super().__init__(self, wheels)
+        self._mountainBike = mountainBike
+
+    @property
+    def mountainBike(self):
+        return self._mountainBike
+
+    @mountainBike.setter
+    def mountainBike(self, new_mountainBike):
+        self._mountainBike = new_mountainBike
+
+    @mountainBike.deleter
+    def mountainBike(self):
+        del self._mountainBike
+
+    def __turnLeft(self):
+        print("Turning left.")
+
+    def __turnRight(self):
+        print("Turning left.")
+
+    def __accelerate(self):
+        print("Pedaling faster.")
+
+    def __brake(self):
+        print("Braking.")
+
+    def ride(self):
+        for i in range(10):
+            choice = random.randint(0,3)
+            if choice == 0:
+                self.__turnLeft()
+            elif choice == 1:
+                self.__turnRight()
+            elif choice == 2:
+                self.__accelerate()
+            elif choice == 3:
+                self.__brake()
+            time.sleep(1)
+
+class Boat(Vehicle):
+    def __init__(self, weight, length, wheels = 0):
+        super().__init__(self, wheels)
+        self._weight = weight
+        self._length = length
+
+    @property
+    def weight(self):
+        return self._weight
+
+    @weight.setter
+    def weight(self, new_weight):
+        self._weight = new_weight
+
+    @weight.deleter
+    def weight(self):
+        del self._weight
+
+    @property
+    def length(self):
+        return self._length
+
+    @length.setter
+    def length(self, new_length):
+        self._length = new_length
+
+    @length.deleter
+    def length(self):
+        del self._length
+
+    def goTopSpeed(self):
+        print("You're going " + str((self._length * 1000)/self._weight) + " knots")
+
+class HotAirBalloon(Vehicle):
+    def __init__(self, color, wheels = 0):
+        super().__init__(self, wheels)
+        self._color = color
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, new_color):
+        self._color = new_color
+
+    @color.deleter
+    def color(self):
+        del self._color
+
+    def pop(self):
+        print("A huge, sad " + self._color + " plastic bag floats to the ground...")
